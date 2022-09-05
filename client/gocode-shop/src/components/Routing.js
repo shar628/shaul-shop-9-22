53ca2ba9de2924775c8eecbc24ca278ed2374977
shar628 <99888509+shar628@users.noreply.github.com>
2022-09-05 22:00:46 +0300

    Added routing from render website

--- client/gocode-shop/src/components/Routing.js
+++ client/gocode-shop/src/components/Routing.js
@@ -20,7 +20,9 @@ const Routing = () => {
             // const response = await fetch('https://fakestoreapi.com/products')
             // const dataFromResponse = await response.json()
             // setProductData(dataFromResponse);
-            const productsUrl = "http://localhost:7000/api/products";
+            // const productsUrl = "http://localhost:7000/api/products";
+            const productsUrl = "http://shaul-go-shop.onrender/api/products";
+
             const response = await axios.get(productsUrl);
             console.log(response)
             const data = await response.data;
